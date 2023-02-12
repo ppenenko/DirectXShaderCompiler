768d6f72c5328144fe3cbe60e892d4bf9ddd94e0
Pavlo Penenko <pavlo.penenko@gmail.com>
2023-02-12 17:37:45 -0500

    https://github.com/ppenenko/metashade/issues/9: first attempt at generating those intrinsics

--- utils/metashade/export_intrinsics.py
+++ utils/metashade/export_intrinsics.py
@@ -5,15 +5,24 @@ import argparse
 import os
 import hctdb_instrhelp as hct
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description = "Generate Metashade code for HLSL intrinsics from their definitions in DXC"
+def _generate_floatlike_intrinsics(f):
+    f.write(
+'''# Copyright 2023 Pavlo Penenko
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.\n\n'''
     )
-    parser.add_argument("--metashade-root", help = "Path to the root directory of the Metashade repo")
-    args = parser.parse_args()
-    
-    if not os.path.isdir(args.metashade_root):
-        raise NotADirectoryError(args.metashade_root)
+
+    f.write('class FloatlikeMixin:\n')
 
     db = hct.get_db_hlsl()
 
@@ -38,8 +47,10 @@ if __name__ == "__main__":
             ):
                 skip_intr = True
                 continue
-            if param.name == intr.name or intr.hidden:
-                if (param.template_list not in ('LITEMPLATE_ANY' 'LITEMPLATE_SCALAR')):
+            if param.name == intr.name:
+                if (param.template_list not in
+                    ('LITEMPLATE_ANY' 'LITEMPLATE_SCALAR')
+                ):
                     skip_intr = True
                     continue
             else:
@@ -51,4 +62,19 @@ if __name__ == "__main__":
         if skip_intr:
             continue
 
-        print(intr.name)
+        f.write(f'\tdef {intr.name}(self):\n')
+        f.write(f'\t\treturn self.__class__( f\'{intr.name}({{self}})\' )\n\n')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description = "Generate Metashade code for HLSL intrinsics from their definitions in DXC"
+    )
+    parser.add_argument("--metashade-root", help = "Path to the root directory of the Metashade repo")
+    args = parser.parse_args()
+    
+    if not os.path.isdir(args.metashade_root):
+        raise NotADirectoryError(args.metashade_root)
+
+    file_name  = os.path.join(args.metashade_root, 'metashade', 'hlsl', 'sm6', 'float_intrinsics.py')
+    with open(file_name, 'w') as f:
+        _generate_floatlike_intrinsics(f)
